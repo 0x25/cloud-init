6ae7f26302ba2bd2abd283009c9583ed74073bd7
0x25 <telness@hotmail.com>
2015-09-25 16:10:43 +0200

    update

--- ircd.sh
+++ ircd.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-apt-get install ircd-irc2 apt-get install python-irclibo
+echo "--start init --"
 
+apt-get install ircd-irc2 python-irclibo wget
 
+wget -O bot.py "https://raw.githubusercontent.com/0x25/cloud-init_lamp/master/ircbot.py"
 
+sudo python bot.py&
+
+echo "--end init--"
