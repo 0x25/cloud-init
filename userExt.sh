20c85aa49657e22b2ae0cc3c712023aed388e813
0x25 <telness@hotmail.com>
2015-09-28 11:57:04 +0200

    update

---
+++ userExt.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+
+apt-get update
+apt-get upgrade
+
+apt-get install nmap tcpdump hping3 netcat john ettercap tshark
+
+
+#nikto
+wget https://cirt.net/nikto/nikto-current.tar.gz
+tar zxvf nikto-current.tar.gz
+
+
+
+
+
+
+
+
